0e0a4ce30c782e89847af147f28eb0c714c95e34
mrhobby7 <satyambup@gmail.com>
2025-04-27 12:56:27 -0700

    pushing landing page changes

---
+++ FrontEnd/src/components/MetricsGuide.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Metrics Guide</h1>
+    <p>Understand how different software quality metrics are calculated and interpreted.</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MetricsGuide',
+};
+</script>
